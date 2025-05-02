066bc79073a08e2bc6d58f3a57319d42ddc6ee11
Cauã Lucca <caualucca.dev@gmail.com>
2025-05-01 22:18:34 -0300

    add header mobile design

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,16 @@
 import { CreateSideBar } from "./components/sideBar.js";
+import { headerMobile } from "./components/headerMobile.js";
 
 
 const bodyRoot = document.querySelector('body');
 
 
 window.addEventListener('load', () =>{
+    let screenSize = window.innerWidth;
+
+    bodyRoot.appendChild(headerMobile());
     bodyRoot.appendChild(CreateSideBar());
+
+    
 
 })
---
+++ scripts/components/headerMobile.js
@@ -0,0 +1,117 @@
+// atomic developing: smaller -> bigger 
+
+
+function buttonMenu(){
+    const button = document.createElement('button');
+    button.setAttribute('aria-label', 'Abrir menu');
+    button.classList.add('mobileIcons')
+    const idButton = 'menu_img'
+
+    button.classList.add('buttonMenu');
+
+    button.innerHTML = 
+    `
+    <img src="../assets/icons/menu_default.png" id="menu_img" alt="menubutton">
+    
+    `;
+
+    return button
+
+};
+
+function logoHeader(){
+    const imgLogo = document.createElement('img');
+    imgLogo.setAttribute('src', '../assets/images/Logo_Nexaas.png');
+    imgLogo.setAttribute('alt', 'Nexaas logo');
+
+    return imgLogo;
+};
+
+function notificationButton(){
+    const buttonNotification = document.createElement('button');
+    buttonNotification.setAttribute('aria-label', 'Abrir notificações');
+    buttonNotification.classList.add('mobileIcons');
+    buttonNotification.classList.add('bellContainer')
+    const notificationID = 'notification_img';
+
+    buttonNotification.innerHTML=
+    `
+    <img src="../assets/icons/notification dark.png" id=${notificationID}>
+    `;
+
+    return buttonNotification;
+}
+
+function profilePicture(){
+    const profileImg = document.createElement('img');
+    profileImg.setAttribute('alt', 'Foto de perfil');
+    profileImg.setAttribute('src', '../assets/images/avatar-admin.png');
+    profileImg.classList.add('imageProfile')
+
+    const chevron_icon = document.createElement('img');
+    chevron_icon.setAttribute('src', '../assets/icons/chevron dark.png');
+    chevron_icon.setAttribute('id', 'mobileChevron');
+    chevron_icon.classList.add('mobileChevron');
+
+    const profileContainer = document.createElement('aside');
+    profileContainer.classList.add('profileContainer');
+
+    profileContainer.appendChild(profileImg);
+    profileContainer.appendChild(chevron_icon);
+
+    return profileContainer;
+};
+
+function asideHeader_mobile() {
+    const userNav = document.createElement('div');
+    userNav.classList.add('userNav_mobile');
+
+    userNav.appendChild(notificationButton());
+    userNav.appendChild(profilePicture());
+
+    return userNav;
+};
+
+function headerMobile_footer(){
+    const footerHeader = document.createElement('footer');
+    footerHeader.classList.add('searchContainer');
+
+    const searchIcon = document.createElement('img');
+    searchIcon.setAttribute('src', '../assets/icons/Search.png');
+    searchIcon.classList.add('mobileIcons');
+    searchIcon.classList.add('searchIcon')
+
+    const searchBar = document.createElement('input');
+    searchBar.setAttribute('type', 'search');
+    searchBar.setAttribute('id', 'searchBar');
+    searchBar.setAttribute('placeholder', 'Busque por clientes')
+
+    footerHeader.appendChild(searchIcon);
+    footerHeader.appendChild(searchBar);
+
+    return footerHeader;
+}
+
+function navigationUser(){
+    const navUser = document.createElement('section');
+    navUser.classList.add('navUser');
+
+    navUser.appendChild(buttonMenu());
+    navUser.appendChild(logoHeader());
+    navUser.appendChild(asideHeader_mobile());
+
+    return navUser;
+};
+
+export function headerMobile(){
+    const headerMobile = document.createElement('header');
+    headerMobile.classList.add('headerMobile');
+
+    headerMobile.appendChild(navigationUser());
+    headerMobile.appendChild(headerMobile_footer());
+
+    return headerMobile;
+}
+
+
+
